6dee8202cf4053ac6a17547437a07944636b1f13
Jamal <cory.jy.lee@gmail.com>
2017-10-18 14:40:04 -0400

    killed random test code

--- src/index.js
+++ src/index.js
@@ -5,30 +5,3 @@ import App from './App';
 import superagent from 'superagent';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-fetchHotelData();
-
-
-export function fetchHotelData(){
-  const body = {
-    city : 'Toronto',
-    checkin : '2018-08-22',
-    checkout : '2018-08-23',
-    provider : 'retail'
-  };
-
-  return new Promise((resolve, reject) => {
-    superagent
-    .post(`https://460d7740.ngrok.io/interview/hotels`)
-    .set('Accept', 'application/json')
-    .set('Content-Type', 'application/json')
-    .send(body)
-    .end((err, res) => {
-      console.log(res);
-      if (err) {
-        reject(err)
-      } else {
-        resolve(res.body);
-      }
-    })
-  });
-}

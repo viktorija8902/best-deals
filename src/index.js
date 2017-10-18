3e95d6e904fc087a9d10ad202f6a1bca4961e631
Jamal <cory.jy.lee@gmail.com>
2017-10-18 14:01:36 -0400

    rev-1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import superagent from 'superagent';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+fetchHotelData();
+
+
+export function fetchHotelData(){
+  const body = {
+    city : 'Toronto',
+    checkin : '2018-08-22',
+    checkout : '2018-08-23',
+    provider : 'retail'
+  };
+
+  return new Promise((resolve, reject) => {
+    superagent
+    .post(`https://460d7740.ngrok.io/interview/hotels`)
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send(body)
+    .end((err, res) => {
+      console.log(res);
+      if (err) {
+        reject(err)
+      } else {
+        resolve(res.body);
+      }
+    })
+  });
+}

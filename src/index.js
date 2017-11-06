3e0469eb140959789361232a9294ac27e5ac7536
Viktorija Bezsazna <viktorija.bezsazna@gmail.com>
2017-11-06 13:50:50 -0500

    Update folder structure: styles and js go to separate folders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+import App from './js/App';
 import superagent from 'superagent';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         To get started, edit <code>src/js/App.js</code> and save to reload.
         </p>
       </div>
     );
